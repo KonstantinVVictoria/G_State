d3b9ad7d391b82ce3ec516003440ea8b79cf96f0
KonstantinTheRed <61480260+KonstantinTheRed@users.noreply.github.com>
2020-08-07 01:42:06 -0700

    updated release

--- README.md
+++ README.md
@@ -1,41 +1,44 @@
-![enter image description here](https://i.imgur.com/CVtR8VU.png)
+a global state management paradigm.
 
 # G_State
 
+![G_State Logo](https://i.imgur.com/CVtR8VU.png)
+
 ## What is G_State?
 
-G_State is a global state management paradigm for react that makes it easy to share properties between components.  
+G_State is a global state management paradigm for react that makes it easy to share properties between components.
 
-In this paradigm, you define a global state, decide and document how it is changed, and render the components that are affected.  
+In this paradigm, you define a global state, decide and document how it is changed, and render the components that are affected.
 
 ## What are the components of G_State?
 
-G_State has three main components:  
+G_State has three main components:
 
-1. **A global state** and all of its **global properties**.  
-2. **Events** that describe how a property is changed.  
-3. **Dependencies** that state which components will be rendered.  
+1. **A global state** and all of its **global properties**.
+2. **Events** that describe how a property is changed.
+3. **Dependencies** that state which components will be rendered.
 
 ## What is the G_State paradigm?
 
-The G_State paradigm is an organizational model where the events that change the global properties are written in the same place where they are defined. This makes it easier to find where things go wrong. For example, if a global property was changed to an unintentional value, you don't have to dig around to find it. It will be in your
-G_State.js file below the global state. Also, changes made to the global properties must be formally described. This helps with debugging and readability.
+The G_State paradigm is an organizational model where the events that change the global properties are written in the same place where the global
+properties are defined. This makes it easier to find where things go wrong. For example, if a global property was changed by an event to an unintentional value, you don't have to dig around to find where the event was written. It will be in your
+G_State.js file below the global state. Also in this paradigm, all the changes made to the global properties are formally described. This helps with debugging and readability.
 
 ## What are the perks of using G_State?
 
-1. **An easy-to-visualize, manage, and organize global state.**  
-2. **Selective and efficient rendering.**  
-3. **Easy-to-follow syntax and declarations that make sense.**  
-4. **Monitor global state events and renders.**  
-5. **Cleaner component definitions**  
-6. **Work with both functional and class components.**  
+1. **An easy-to-visualize, manage, and organize global state.**
+2. **Selective and efficient rendering.**
+3. **Easy-to-follow syntax and declarations that make sense.**
+4. **Monitor global state events and renders.**
+5. **Cleaner component definitions**
+6. **Work with both functional and class components.**
 
 ## How does it work?
 
-You start by creating a global state and defining its properties. The global properties are the properties that are shared by more than one component. You define how these global properties will be changed as method properties of the global properties. In these method properties or "events", you define how a global property value is changed. In your component files, you can define which properties will render a component when they are changed, and you can also call the method events whenever needed and access the global properties freely.
+You start by creating a global state and defining its properties. The global properties are the properties that are shared by more than one component. You define how these global properties will be changed as method properties of the global properties. In your component files, you define which properties will render a component when they are changed, and you can also call the method events whenever needed and access the global properties freely.
 
 **Creating a global state**
-Create a global state is as easy as creating an object. Create a G_State.js file under your src folder. In the G_State.js file import the G_State library. Define a state object with the global properties needed. Afterwards, define the global properties further, and design events for them.
+Creating a global state is as easy as creating an object. Create a G_State.js file under your src folder. In the G_State.js file import the G_State library. Define a state object with the global properties needed. Afterwards, define the global properties further, and design events for them.
 
 **Designing Events**
 You can design events explicitly or implicitly. An explicit definition describes a future state. An implicit
@@ -43,37 +46,43 @@ definition calls the embedded changeTo method in a property. Note, however; that
 
     //In G_State.js
     import G_State from "g_state-management"
-    let State = {
+    let GlobalState = { // Create a global state
         property: {
     	    value : 0 ,
     	    increment : () => {},
         },
         favoriteNumber: () => {}
     }
-    //Define properties further.
-    State.favoriteNumber = () => {
-        return State.property.increment() + State.property.value + 3
+    //Define the global properties further
+    GlobalState.favoriteNumber = () => {
+        return GlobalState.property.increment() + GlobalState.property.value + 3
     }
 
     //Design events below explicitly or implicitly.
+
+    GlobalState.property.increment = () => {
     //Explicitly:
-    State.property.increment () => {
         let newState = {} //Define a new state object.
         newState.property = () => { /
         /*Add a method property that is the
         same name of the property being changed.*/
 
-    	    State.property.value++; //Describe how it is changed
+    	    GlobalState.property.value++; //Describe how it is changed
     	    return "incremented_property_value //Name the event
         }
-        G_State.changesTo(newState)//Commit the event.
+        G_State.changesTo(newState)//Commit the event using the G_State library.
+    //----------//
     }
-    //Implicitly: *note that it only works with properties that have a values property
-    State.property.increment () => {
-    	let newValue = State.property.value++
-    	State.property.changesTo("incremented",newValue") //changesTo method can be overrided.
+
+    GlobalState.property.increment = () => {
+        //Implicitly: *note that it only works with properties that have a value property
+    	let newValue = GlobalState.property.value++
+    	GlobalState.property.changesTo("incremented",newValue")
+        //The embedded method changesTo is only available to properties that have a value property.
+        //It can be overrided.
+        //-------------------//
     }
-    export default State;
+    export default GlobalState;
 
 **Accessing global properties**
 
@@ -82,13 +91,15 @@ definition calls the embedded changeTo method in a property. Note, however; that
     import G_State from "g_state-management";
 
     let {property} = G_State.now
-    //Note that G_State.now is equivalent to the State object in G_State.js
+    //Note that G_State.now is equivalent to the GlobalState object in G_State.js
+
     const Component = () => {
-    return(<button onMouseDown={property.increment}>+</button>)
+        return(<button onMouseDown={property.increment}>+</button>)
     }
     export default Component;
 
 **Updating the components that need to be updated**
+//Note: If a component doesn't need to be updated, you do not have to do this.
 Functional Components:
 
     //In DependantFunctonalComponent.jsx
@@ -97,16 +108,16 @@ Functional Components:
     import G_State from "g_state-management";
 
     let {property, favoriteNumber} = G_State.now
-    let dependancies = G_State.link("property")
+    let dependancies = G_State.link("property") //This component will rerender if the property property is changed.
 
     const DependantFunctonalComponent = () => {
         return(<div>{favoriteNumber()}</div>)
         //Whenever property is changed by the other component,
-        //we need this function to rerender.
+        //we need this component to rerender.
     }
 
     export default G_State.updates(DependantFunctonalComponent, dependancies);
-    //This connects the component to the global state and define which properties affects it.
+    //This connects the component to the global state and defines which properties affects it.
 
 Class Components:
 
@@ -127,7 +138,7 @@ Class Components:
     		//You can define events implicitly and explicitly in components as well
     	}
     	render(){
-    		return(<div onMouseDown = {this.pressed} > </div>)
+    		return(<div onMouseDown = {this.pressed} >{property.value} </div>) //Everytime property changes, we need to rerender the component to display the latest value.
     	}
     }
 
@@ -135,26 +146,34 @@ Class Components:
 
 # Documentation
 
+example: https://github.com/KonstantinVVictoria/webscrapeProj
+
 **G_State.now**  
--- The global state.  
+-- The global state.
 
 **G_State.updates( component, dependancies )**  
--- Connects the component to the global state. Adds a listener that listens to change events.  
--- Only the components that need to be rerendered needs to this statement.  
--- Works for functional and class components.  
+-- Connects the component to the global state. Creates a listener that listens to any G_State.changesTo(newState) calls.  
+-- Only the components that need to be rerendered needs to this statement in their definition.  
+-- Works for functional and class components.
 
 **G_State.link( "property_1", "property_2", ... )**  
--- Returns an object that describes the dependancies.  
+-- Returns an object that describes the dependancies.
 
 **G_State.changesTo( newState )**  
--- Commits the event, broadcasts the changes to all components, and renders the components that have  
-the property changed listed in its dependency.  
+-- Commits the events described in a newState object, broadcasts the changes to all components listening, and renders the components that have  
+the name of the properties that were changed by the events listed in its dependencies object.
 
-**G_State.now["property"].changesTo( newState )**  
--- Commits the event, broadcasts the changes to all components, and renders the components that have  
-the property changed listed in its dependency.  
+**G_State.now["property"].changesTo( "event name", newValue )**  
+-- This is an embedded method. Whenever a property has a value property, this method is automatically embedded.
+-- Commits the event, broadcasts the changes of a property to all components, and rerenders the components that have the name of the properties that were changed by the events listed in its dependencies object.
+-- This only works if the property has a value property.
+-- This can only change a property's value property
 
-**G_State.debug( {setting1: true/false}, {setting2: true/false} )**  
+**G_State.debug( {setting1: true/false, setting2: true/false} )**  
 -- When activated, it will store all the events descriptions into window.G_Events, and the current state into window.G_State.  
--- To read the event description or current state, simply console log the window objects.  
--- The only settings currently available is {live: true/false} which will console log the events when they are committed.  
+-- To read the event descriptions or current state, simply console log their corresponding window objects.  
+-- The only settings currently available is {live: true/false, showMount: true/false} which will console log the events and mounts when they are committed.
+
+# Bugs and Collaboration
+
+Want to report a bug or interested in collaborating? Email me at: scythodemes@gmail.com.
--- index.js
+++ index.js
@@ -4,133 +4,133 @@ try {
   var GlobalState = require("../../src/G_State.js");
 } catch (ex) {
   console.error(
-    "Please add a GlobalState.js file containing an object with all the global properties"
+    "Please add a G_State.js file containing an object with all the global properties"
   );
 }
 
-if (GlobalState === undefined)
-  throw new Error("There is an error in G_State.js");
-else {
-  var now = GlobalState.default;
-  const debug = (args) => {
-    if (args) {
-      Object.keys(args).forEach((arg) => {
-        if (debugArgList[arg] !== undefined) debugArgList[arg] = arg;
-        else console.log("not a valid setting");
-      });
-    }
-    debugArgList.isDebugging = true;
-    window.G_events = events;
-    window.G_State = now;
-  };
-  const changesTo = (changes) => {
-    let changedProperties = [];
-    Object.entries(changes).forEach((change) => {
-      changedProperties.push(change[0]);
-      currentEvent = change[0];
-
-      if (debugArgList.isDebugging) {
-        createEvent(change);
-        debug();
-      } else change[1]();
+var now = GlobalState.default;
+const debug = (args) => {
+  if (args) {
+    Object.keys(args).forEach((arg) => {
+      if (debugArgList[arg] !== undefined) debugArgList[arg] = arg;
+      else console.log("not a valid setting");
     });
-    document.dispatchEvent(
-      new CustomEvent("stateChanged", {
-        detail: {
-          changedProperties: changedProperties,
-          now: now,
-        },
-      })
-    );
-  };
+  }
+  debugArgList.isDebugging = true;
+  window.G_events = events;
+  window.G_State = now;
+};
+const changesTo = (changes) => {
+  let changedProperties = [];
+  Object.entries(changes).forEach((change) => {
+    changedProperties.push(change[0]);
+    currentEvent = change[0];
 
-  Object.entries(now).forEach((property) => {
-    if (typeof property[1] === "object" && property[1].hasOwnProperty("value"))
-      now[property[0]].changesTo = (event, value) => {
-        let newState = {};
-        newState[property[0]] = {};
-        newState[property[0]] = () => {
-          now[property[0]].value = value;
-          return event + "_" + property[0];
-        };
-        changesTo(newState);
+    if (debugArgList.isDebugging) {
+      createEvent(change);
+      debug();
+    } else change[1]();
+  });
+  document.dispatchEvent(
+    new CustomEvent("stateChanged", {
+      detail: {
+        changedProperties: changedProperties,
+        now: now,
+      },
+    })
+  );
+};
+
+Object.entries(now).forEach((property) => {
+  if (typeof property[1] === "object" && property[1].hasOwnProperty("value"))
+    now[property[0]].changesTo = (event, value) => {
+      let newState = {};
+      newState[property[0]] = {};
+      newState[property[0]] = () => {
+        now[property[0]].value = value;
+        return event + "_" + property[0];
       };
+      changesTo(newState);
+    };
+});
+
+var events = [];
+
+var debugArgList = {
+  isDebugging: false,
+  showMount: false,
+  live: false,
+};
+
+let currentEvent = "";
+
+const show = () => {
+  if (now) return now;
+  else return "There are no properties in the global state.";
+};
+
+const createEvent = (change) => {
+  if (debugArgList.isDebugging) {
+    let event = {
+      event: change[1](),
+    };
+    event[
+      typeof change[0] !== "string"
+        ? typeof change[0] === "object" && change[0].G_ID
+          ? "component"
+          : change[0]
+        : change[0]
+    ] =
+      typeof change[0] === "string" && !change[0].G_ID
+        ? State.now[change[0]]
+        : typeof change[0] == "object" && change[0].G_ID
+        ? change[0]._reactInternalFiber._debugSource.fileName +
+          ":" +
+          change[0]._reactInternalFiber._debugSource.lineNumber
+        : State.now[change[0]]
+        ? State.now[change[0]]
+        : "";
+    //Why you should not write logic this
+    events.push(event);
+    if (debugArgList.live)
+      console.log(events.length, events[events.length - 1]);
+  }
+};
+
+const updateState = (componentLink, changes) => {
+  let isChangedComponent = false;
+  changes.changedProperties.forEach((property) => {
+    if (componentLink.G_dependancies.hasOwnProperty(property)) {
+      isChangedComponent = true;
+      componentLink.component.state.G_dependancies[property] = true;
+    }
   });
 
-  var events = [];
+  if (isChangedComponent) {
+    componentLink.component.setState(componentLink.component.state);
+  }
+};
 
-  var debugArgList = {
-    isDebugging: false,
-    showInitialMount: false,
-    live: false,
-  };
+const commit = (actions) => {
+  if (actions) {
+    Object.entries(actions).forEach((action) => {
+      let newState = {};
+      newState[action[0]] = action[1];
+      let event = Object.keys(newState)[0];
+      if (currentEvent !== event && currentEvent !== "initial_mount") {
+        currentEvent = Object.keys(newState)[0];
 
-  let currentEvent = "";
-
-  const show = () => {
-    if (now) return now;
-    else return "There are no properties in the global state.";
-  };
-
-  const createEvent = (change) => {
-    if (debugArgList.isDebugging) {
-      let event = {
-        event: change[1](),
-      };
-      event[
-        typeof change[0] !== "string"
-          ? typeof change[0] === "object" && change[0].G_ID
-            ? "component"
-            : change[0]
-          : change[0]
-      ] =
-        typeof change[0] === "string" && !change[0].G_ID
-          ? State.now[change[0]]
-          : typeof change[0] == "object" && change[0].G_ID
-          ? change[0]._reactInternalFiber._debugSource.fileName +
-            ":" +
-            change[0]._reactInternalFiber._debugSource.lineNumber
-          : State.now[change[0]]
-          ? State.now[change[0]]
-          : "";
-      //Why you should not write logic this
-      events.push(event);
-      if (debugArgList.live)
-        console.log(events.length, events[events.length - 1]);
-    }
-  };
-
-  const updateState = (componentLink, changes) => {
-    let isChangedComponent = false;
-    changes.changedProperties.forEach((property) => {
-      if (componentLink.G_dependancies.hasOwnProperty(property)) {
-        isChangedComponent = true;
-        componentLink.component.state.G_dependancies[property] = true;
+        changesTo(newState);
       }
     });
+  }
+};
 
-    if (isChangedComponent) {
-      componentLink.component.setState(componentLink.component.state);
-    }
-  };
-
-  const commit = (actions) => {
-    if (actions) {
-      Object.entries(actions).forEach((action) => {
-        let newState = {};
-        newState[action[0]] = action[1];
-        let event = Object.keys(newState)[0];
-        if (currentEvent !== event && currentEvent !== "initial_mount") {
-          currentEvent = Object.keys(newState)[0];
-
-          changesTo(newState);
-        }
-      });
-    }
-  };
-
-  const updates = (component, state, actions) => {
-    if (debugArgList.showInitialMount) {
+const updates = (component, state, actions) => {
+  if (typeof component === "function") {
+    return instantiate(component, state, actions);
+  } else if (typeof component === "object") {
+    if (debugArgList.showMount) {
       currentEvent = "initial_mount";
       let initialEvent = [
         "initial_mount",
@@ -141,109 +141,105 @@ else {
 
       createEvent(initialEvent);
     }
-    if (typeof component === "function") {
-      return instantiate(component, state, actions);
-    } else if (typeof component === "object") {
-      if (!component.G_ID) component.G_ID = component.constructor.name;
-      let cachedShouldComponentUpdate = component.shouldComponentUpdate
-        ? component.shouldComponentUpdate
-        : () => {};
+    if (!component.G_ID) component.G_ID = component.constructor.name;
+    let cachedShouldComponentUpdate = component.shouldComponentUpdate
+      ? component.shouldComponentUpdate
+      : () => {};
 
-      component.shouldComponentUpdate = () => {
-        let hasNotUpdated = false;
-        Object.keys(component.state.G_dependancies).forEach((dependancy) => {
-          if (component.state.G_dependancies[dependancy]) {
-            hasNotUpdated = true;
-            createEvent([
-              componentLink.component,
-              () => {
-                return "rendering " + componentLink.component.G_ID;
-              },
-            ]);
-          }
-        });
-        cachedShouldComponentUpdate();
-        return hasNotUpdated;
-      };
-
-      let cachedComponentDidUpdate = component.componentDidUpdate
-        ? component.componentDidUpdate
-        : () => {};
-      component.componentDidUpdate = () => {
-        cachedComponentDidUpdate();
-        Object.keys(component.state.G_dependancies).forEach((property) => {
-          component.state.G_dependancies[property] = false;
-        });
-        if (actions) {
-          commit(actions);
+    component.shouldComponentUpdate = () => {
+      let hasNotUpdated = false;
+      Object.keys(component.state.G_dependancies).forEach((dependancy) => {
+        if (component.state.G_dependancies[dependancy]) {
+          hasNotUpdated = true;
         }
-      };
+      });
+      cachedShouldComponentUpdate();
+      return hasNotUpdated;
+    };
 
-      let componentLink = {
-        component: component,
-        G_dependancies: component.state.G_dependancies,
-      };
-
-      let heardUpdate = (event) => {
-        return updateState(componentLink, event.detail);
-      };
-
-      document.addEventListener("stateChanged", heardUpdate);
-      component.componentWillUnmount = () => {
-        document.removeEventListener("stateChanged", heardUpdate);
-      };
-    }
-  };
-
-  const instantiate = (ChildComponent, G_dependancies, actions) => {
-    class Comp extends Component {
-      constructor(props) {
-        super(props);
-        this.state = {};
-        this.state.G_dependancies = G_dependancies;
-        this.G_ID = ChildComponent.name;
-        updates(this);
-      }
-
-      componentDidUpdate() {
+    let cachedComponentDidUpdate = component.componentDidUpdate
+      ? component.componentDidUpdate
+      : () => {};
+    component.componentDidUpdate = () => {
+      cachedComponentDidUpdate();
+      Object.keys(component.state.G_dependancies).forEach((property) => {
+        component.state.G_dependancies[property] = false;
+      });
+      createEvent([
+        componentLink.component,
+        () => {
+          return "rendering " + componentLink.component.G_ID;
+        },
+      ]);
+      if (actions) {
         commit(actions);
       }
+    };
 
-      render() {
-        return React.createElement(
-          React.Fragment,
-          null,
-          React.createElement(ChildComponent, this.props)
-        );
-      }
+    let componentLink = {
+      component: component,
+      G_dependancies: component.state.G_dependancies,
+    };
+
+    let heardUpdate = (event) => {
+      return updateState(componentLink, event.detail);
+    };
+
+    document.addEventListener("stateChanged", heardUpdate);
+    component.componentWillUnmount = () => {
+      document.removeEventListener("stateChanged", heardUpdate);
+    };
+  }
+};
+
+const instantiate = (ChildComponent, G_dependancies, actions) => {
+  class Comp extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {};
+      this.state.G_dependancies = G_dependancies;
+      this.G_ID = ChildComponent.name;
+      updates(this);
     }
-    return Comp;
-  };
-  const addProperty = (propertyName, property) => {
-    now[propertyName] = property;
-  };
 
-  const removeProperty = (propertyName) => {
-    delete now[propertyName];
-  };
+    componentDidUpdate() {
+      commit(actions);
+    }
 
-  const link = (...args) => {
-    let dependancies = {};
-    args.forEach((dependancy) => {
-      dependancies[dependancy] = null;
-    });
-    return dependancies;
-  };
+    render() {
+      return React.createElement(
+        React.Fragment,
+        null,
+        React.createElement(ChildComponent, this.props)
+      );
+    }
+  }
+  return Comp;
+};
+const addProperty = (propertyName, property) => {
+  now[propertyName] = property;
+};
 
-  var State = {
-    now,
-    link,
-    show,
-    updates,
-    addProperty,
-    removeProperty,
-    changesTo,
-    debug,
-  };
-}
+const removeProperty = (propertyName) => {
+  delete now[propertyName];
+};
+
+const link = (...args) => {
+  let dependancies = {};
+  args.forEach((dependancy) => {
+    dependancies[dependancy] = null;
+  });
+  return dependancies;
+};
+
+var State = {
+  now,
+  link,
+  show,
+  updates,
+  addProperty,
+  removeProperty,
+  changesTo,
+  debug,
+};
 export default State;
